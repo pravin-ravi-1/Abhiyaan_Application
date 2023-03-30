f88e15ed6076ce7e30187c1dbda129a6996f07d4
pravin-ravi-1 <129218927+pravin-ravi-1@users.noreply.github.com>
2023-03-30 19:07:49 +0530

    Add files via upload

---
+++ Q2/Subtask-1/Publisher.py
@@ -0,0 +1,25 @@
+import rospy
+from std_msgs.msg import String
+
+def talker():
+
+    # rostopic - 'chatter'
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+
+        # msg - ‘NO.1 CFI TEAM’
+        message = "NO.1 CFI TEAM"
+
+        rospy.loginfo(message)
+        pub.publish(message)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
